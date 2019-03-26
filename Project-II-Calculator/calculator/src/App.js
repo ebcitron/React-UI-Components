0011f8d30dd760bbb8196e404edef5916169956d
Eli Citron <ebcitron@gmail.com>
2019-03-26 16:13:38 -0500

    Calculator Design Complete

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 
 const App = () => {
   return (
-    <div>
+    <div className = "App">
       <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+      <div className="Calculator">
+        <div className="Display">
+        <CalculatorDisplay text = 'testing' />
+        </div>
+        <div id="row">
+       
+          <ActionButton buttonStyle = "Action" text="clear" /> 
+          <NumberButton buttonStyle = "Red" text="/" />
+        </div>
+        <div id="row">
+        <NumberButton buttonStyle = "Regular" text="1" />
+      <NumberButton buttonStyle = "Regular"  text="2" />
+          <NumberButton buttonStyle = "Regular" text="3" />
+          <NumberButton buttonStyle = "Red" text="X" />
+        </div>
+        <div id="row">
+        <NumberButton buttonStyle = "Regular"  text="1" />
+      <NumberButton buttonStyle = "Regular" text="2" />
+          <NumberButton buttonStyle = "Regular" text="3" />
+          <NumberButton buttonStyle = "Red" text="-" />
+        </div>
+        <div id="row">
+        <NumberButton buttonStyle = "Regular"  text="1" />
+      <NumberButton buttonStyle = "Regular" text="2" />
+          <NumberButton buttonStyle = "Regular" text="3" />
+          <NumberButton buttonStyle = "Red" text="+" />
+        </div>
+        <div id="row">
+        <ActionButton buttonStyle = "Action" text = "0" />
+          <NumberButton buttonStyle = "Red" text="=" />
+        </div>
+      </div>
     </div>
+  
   );
 };
 
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+
+
+const CalculatorDisplay = props => {
+   return( <button className="display">{props.text}</button>
+   )}
+
+export default CalculatorDisplay;
